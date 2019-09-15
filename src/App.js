d62a71ee42dfe00db5e9d257ef8c5dda5291c80a
Kyle Richardson <kyle.richardson1@gmail.com>
2019-09-15 16:08:52 -0500

    working events

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [memberList, setMemberList] = useState([{
+    name: 'kyle',
+    age: '31'
+    }, {
+    name: 'lauren',
+    age: '29'
+  }])
+  const [user, setUser] = useState({
+    name: '',
+    age: ''
+  })
+
+  useEffect(() => {
+  },[memberList])
+
+    const handleChange = event => {
+      const name = event.target.name
+      const value = event.target.value;
+      setUser({ ...user, [name]: value});
+    }
+
+    const handleSubmit = event => {
+      event.preventDefault()
+      setMemberList(old => [...old, user])
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
+      <form onSubmit={(e)=> handleSubmit(e)}>
+        <input type="text" name="name" value={user.name} onChange={e=> handleChange(e)}/>
+        <input type="text" name="age" value={user.age} onChange={e=> handleChange(e)}/>
+        <button>Add to member list</button>
+      </form>
     </div>
   );
 }
